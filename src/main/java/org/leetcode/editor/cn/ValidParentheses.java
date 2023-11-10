1a75e21cd80f8655be43e9ef757ab5cf41ae3265
AbirdWhocannotFly <51596617+AbirdWhocannotFly@users.noreply.github.com>
2023-11-10 23:53:35 +0800

    Add files via upload
    init

---
+++ src/main/java/org/leetcode/editor/cn/ValidParentheses.java
@@ -0,0 +1,72 @@
+
+package org.leetcode.editor.cn;
+/**
+*题目编号:20
+*题目标题:有效的括号
+*2023-11-10 23:15:11
+*/
+//给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。 
+//
+// 有效字符串需满足： 
+//
+// 
+// 左括号必须用相同类型的右括号闭合。 
+// 左括号必须以正确的顺序闭合。 
+// 每个右括号都有一个对应的相同类型的左括号。 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "()"
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "()[]{}"
+//输出：true
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：s = "(]"
+//输出：false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 10⁴ 
+// s 仅由括号 '()[]{}' 组成 
+// 
+//
+// Related Topics栈 | 字符串 
+//
+// 👍 4245, 👎 0bug 反馈 | 使用指南 | 更多配套插件 
+//
+//
+//
+//
+
+public class ValidParentheses{
+	public static void main(String[] args) {
+	Solution solution = new Solution();
+	// System.out.println(solution.validParentheses());
+	// TO TEST
+}
+}
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isValid(String s) {
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
